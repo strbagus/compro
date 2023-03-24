e6857fa08e88b329299c3d3bce0ec8a7c7fb507e
strbagus <satrio.baguswbw@gmail.com>
2023-03-24 23:53:16 +0700

    main layout & tailwind color pallete

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,13 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+        primary: '#FC0000',
+        secondary: '#1C1917',
+        ternary: '#57534E',
+			},
+		},
 	},
 	plugins: [],
 }
